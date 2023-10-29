3671a92cfc77bb9d8520aca606ecf264da6d6f65
Naveensbn <naveensebastian143@gmail.com>
2023-10-29 21:18:12 +0530

    added

---
+++ RockPaperScissors.py
@@ -0,0 +1,54 @@
+
+import random
+while True:
+    choices = ["rock", "paper", "scissors"]
+
+    computer = random.choice(choices)
+
+    player = None
+# or
+# player = 'apple'  # rule: given value doesn't exist in choices
+    while player not in choices:
+        player = input('rock, paper, or scissors?: ').lower()
+    # you want to exit the loop make it false the condition
+
+    if player == computer:
+        print("computer: ", computer)
+        print("player: ", player)
+        print("tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("you lose!")
+        if computer == "scissors":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("you win!")
+    elif player == "paper":
+        if computer == "scissors":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("you lose!")
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("you win!")
+
+    elif player == "scissors":
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("you lose!")
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("you win!")
+
+    play_again = input("play again?(yes/no): ").lower()
+    if play_again != "yes":
+        print("Thank you")
+        break
+print("bye")
+
+
