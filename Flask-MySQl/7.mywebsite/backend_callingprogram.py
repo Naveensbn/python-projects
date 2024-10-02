473f98292862477f184784657f22e3b62e99b5ee
Naveensbn <143919982+Naveensbn@users.noreply.github.com>
2024-10-02 13:47:15 +0530

    Update backend_callingprogram.py

--- Flask-MySQl/7.mywebsite/backend_callingprogram.py
+++ Flask-MySQl/7.mywebsite/backend_callingprogram.py
@@ -1,11 +1,11 @@
 from crud_operation import *
-'''
+
 stu_name=input("enter the name")
 stu_location=input("enter the location")
 mark1=int(input("enter the mark1"))
 mark2=int(input("enter the mark2"))
 mark3=int(input("enter the mark3"))
 insertstudent(stu_name,stu_location,mark1,mark2,mark3)
-'''
+
 readallstudent()
 # readperticularstudent(1)
